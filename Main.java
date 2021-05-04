a5a036e40ecd3d5cd15a7f90c7c63979088dd583
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-05-04 23:54:17 +0200

    TestApp

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Checking the results of the methods.
+        SecondsAndMinutes.getDurationString(65 ,45);
+        SecondsAndMinutes.getDurationString(2967);
+
+
+    }
+}
