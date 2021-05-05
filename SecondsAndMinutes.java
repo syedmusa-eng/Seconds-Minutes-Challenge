43f2ae721943f6a43a6ea120f49a570a09a9fca4
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-05-05 19:05:29 +0200

    Code with some tweeks.
    Added the functionality of a "Final static" variable. also changed the format of the clock showing us the time.

--- SecondsAndMinutes.java
+++ SecondsAndMinutes.java
@@ -1,16 +1,32 @@
 public class SecondsAndMinutes {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
     //Creating the 1st method
     public static int getDurationString(int minutes, int seconds) {
 
         if (minutes < 0 || seconds < 0 || seconds > 59) {
-            System.out.println("Invalid value.");
+            System.out.println(INVALID_VALUE_MESSAGE);
             return -1;
         } else {
             //minutes = (seconds / 60) + minutes;
             int hours = minutes / 60;
             minutes = minutes % 60;
-            System.out.println(hours + "h " + minutes + "m " + seconds + "s");
+
+            String hoursString = hours + "h";
+            if (hours < 10) {
+                hoursString = "0" + hoursString;
+            }
+            String minutesString = minutes + "m";
+            if (minutes < 10) {
+                minutesString = "0" + minutesString;
+            }
+            String secondsString = seconds + "s";
+            if (seconds < 10) {
+                secondsString = "0" + secondsString;
+            }
+
+            System.out.println(hoursString + " " + minutesString + " " + secondsString + "");
             return hours;
         }
     }
@@ -19,7 +35,7 @@ public class SecondsAndMinutes {
     public static int getDurationString(int seconds) {
 
         if (seconds < 0) {
-            System.out.println("Invalid value.");
+            System.out.println(INVALID_VALUE_MESSAGE);
             return -1;
         } else {
             int minutes = seconds / 60;
